2d23533ada038bffc0b0b30f306ddb7d542c3c3e
Roma Malyi <roma.maliy13@gmail.com>
2023-03-07 00:24:37 +0200

    test

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 const gameBoard = document.getElementById('game');
 const cells = document.querySelectorAll(".cell");
-const playerTurn = document.getElementById('player');
 
-let currentPlayer = "x";
-let gameActive = true;
+
+let currentPlayer = "X";
+
 
 const winningPosition = [
     [1,2,3],
@@ -19,5 +19,47 @@ const winningPosition = [
 addEventListener("click", function (event){
     if (event.target.className === "cell" && event.target.textContent===""){
         event.target.innerHTML = currentPlayer;
+       /* if(currentPlayer==='X'){
+            currentPlayer = 'O';
+        }else currentPlayer = 'X';*/
+        currentPlayer = (currentPlayer=='X')? 'O': 'X';
+        checkWin();
+
     }
 })
+
+
+function checkWin(){
+    if (cells[0].innerHTML== cells[1].innerHTML && cells[1].innerHTML==cells[2].innerHTML && cells[0].innerHTML!=''){
+        currentPlayer = (currentPlayer=='X')? 'O': 'X';
+        alert("Win : " + currentPlayer);
+    }
+    else if (cells[3].innerHTML== cells[4].innerHTML && cells[4].innerHTML==cells[5].innerHTML  && cells[5].innerHTML!=''){
+        currentPlayer = (currentPlayer=='X')? 'O': 'X';
+        alert("Win : " + currentPlayer);
+    }
+    else if (cells[6].innerHTML== cells[7].innerHTML && cells[7].innerHTML==cells[8].innerHTML  && cells[7].innerHTML!=''){
+        currentPlayer = (currentPlayer=='X')? 'O': 'X';
+        alert("Win : " + currentPlayer);
+    }
+    else if (cells[0].innerHTML== cells[3].innerHTML && cells[3].innerHTML==cells[6].innerHTML  && cells[6].innerHTML!=''){
+        currentPlayer = (currentPlayer=='X')? 'O': 'X';
+        alert("Win : " + currentPlayer);
+    }
+    else if (cells[1].innerHTML== cells[4].innerHTML && cells[4].innerHTML==cells[7].innerHTML  && cells[4].innerHTML!=''){
+        currentPlayer = (currentPlayer=='X')? 'O': 'X';
+        alert("Win : " + currentPlayer);
+    }
+    else if (cells[2].innerHTML== cells[5].innerHTML && cells[5].innerHTML==cells[8].innerHTML  && cells[2].innerHTML!=''){
+        currentPlayer = (currentPlayer=='X')? 'O': 'X';
+        alert("Win : " + currentPlayer);
+    }
+    else if (cells[0].innerHTML== cells[4].innerHTML && cells[4].innerHTML==cells[8].innerHTML  && cells[0].innerHTML!=''){
+        currentPlayer = (currentPlayer=='X')? 'O': 'X';
+        alert("Win : " + currentPlayer);
+    }
+    else if (cells[2].innerHTML== cells[4].innerHTML && cells[4].innerHTML==cells[6].innerHTML  && cells[4].innerHTML!=''){
+        currentPlayer = (currentPlayer=='X')? 'O': 'X';
+        alert("Win : " + currentPlayer);
+    }
+}
